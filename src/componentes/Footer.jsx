9fc7b0524f64d444b0b6cea8d570d46e0dddf974
Pablo Dominguez <pablo.dm666@gmail.com>
2022-08-27 16:53:02 -0300

    .\.git\SegundoDesafio

---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <hr />
+                <p>CoderHouse 2022</p>
+            </div>
+        )
+    }
+}
+
+export default Footer;
